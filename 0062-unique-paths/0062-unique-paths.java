5341ba8b77ff39aae56feb9a1facebb783f21c3a
AAYUSH garg <77825502+dynoarmy@users.noreply.github.com>
2023-01-29 15:33:57 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (94.79%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        if (m <= 0 || n <= 0) {
+            return 0;
+        }
+        if (m == 1 || n == 1) {
+            return 1;
+        }
+        if (m < n) {
+            uniquePaths(n, m);
+        }
+
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[j] += dp[j - 1];
+            }
+        }
+
+        return dp[n - 1];
+    }
+}
