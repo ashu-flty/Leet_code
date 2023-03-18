05a0e86876ee4296153cf9f4c23b540eaa563d42
AAYUSH garg <77825502+dynoarmy@users.noreply.github.com>
2023-03-19 00:55:50 +0530

    Added solution - LeetHub

---
+++ Distinct Difference - GFG/distinct-difference.java
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int N;
+            N = Integer.parseInt(br.readLine());
+            
+            
+            int[] A = IntArray.input(br, N);
+            
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.getDistinctDifference(N, A);
+            
+            IntArray.print(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static ArrayList<Integer> getDistinctDifference(int N, int[] A) {
+        // code here
+        ArrayList<Integer> al = new ArrayList<>();
+        HashSet<Integer> hs=new HashSet<>();
+        int[] a=new int[N];
+        
+        for(int i=N-1;i>=0;i--){
+            a[i]=hs.size();
+            hs.add(A[i]);
+        }
+        
+        HashSet<Integer> hs1=new HashSet<>();
+        for(int i=0;i<N;i++){
+            al.add(hs1.size()-a[i]);
+            hs1.add(A[i]);
+        }
+        return al;
+    }
+
+
+}
+        
